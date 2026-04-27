974d7d41c63a89c342ced2a912ba48952fa76e62
rwvthr <mrls9@aluno.ifal.edu.br>
2026-04-27 00:12:55 -0300

    save

---
+++ AULAS/AULA 2/exercicios/exercicios.js
@@ -0,0 +1,56 @@
+// Exercício 1
+
+function boasVindas() {
+    alert("Bem-vindo!");
+}
+
+boasVindas();
+
+// Exercício 2
+
+function mostrarNome () {
+    let nome = prompt('Digite seu nome: ');
+    alert('Olá,' + nome + '!');
+}
+
+mostrarNome();
+
+// Exercício 3
+
+function somaDois(x, y) {
+    alert("O resultado da soma entre " + x + " e " + y + " é: " + (x + y));
+}
+
+somaDois(5, 10);
+
+// Exercício 4
+
+function dobro(x) {
+    console.log('O dobre de' + x + ' é:' + (x * 2));
+}
+
+dobro(5);
+
+// Exercício 5
+
+function saudacaoPersonalizada(nome) {
+    let nomeUsuario = prompt('Digite seu nome: ');
+    let idadeUsuario = prompt('Digite sua idade: ');
+    alert('Olá, ' + nomeUsuario + 'voce tem ' + idadeUsuario + ' anos!');
+}
+
+saudacaoPersonalizada();
+
+// Exercício 6
+
+
+
+
+// Exercício 7
+
+// Exercício 8
+
+// Exercício 9
+
+// Exercício 10
+
