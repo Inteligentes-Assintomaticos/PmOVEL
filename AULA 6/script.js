7b1a3cbb3d8c0446f6b3e34c0757656fcd87f6da
Felipe <fgaf1ifal@gmail.com>
2026-05-04 17:14:55 -0300

    AULA 6 - EXEMPLOS

---
+++ AULA 6/script.js
@@ -0,0 +1,131 @@
+// Exemplo 1
+
+let dia_exemplo1 = 3
+
+if (dia_exemplo1 == 1) {
+    console.log('Domingo')
+}
+else if (dia_exemplo1 == 2) {
+    console.log('Segunda-Feira')
+}
+else if (dia_exemplo1 == 3) {
+    console.log('Terça-Feira')
+}
+else if (dia_exemplo1 == 4) {
+    console.log('Quarta-Feira')
+}
+else if (dia_exemplo1 == 5) {
+    console.log('Quinta-Feira')
+}
+else if (dia_exemplo1 == 6) {
+    console.log('Sexta-Feira')
+}
+else if (dia_exemplo1 == 7) {
+    console.log('Sábado')
+}
+else {
+    console.log('Dia inválido')
+}
+
+// Exemplo 2
+
+let dia_exemplo2 = 3
+
+switch (dia_exemplo2) {
+    case 1:
+        console.log('Domingo')
+        break
+    case 2:
+        console.log('Segunda-Feira')
+        break
+    case 3:
+        console.log('Terça-Feira')
+        break
+    case 4:
+        console.log('Quarta-Feira')
+        break
+    case 5:
+        console.log('Quinta-Feira')
+        break
+    case 6:
+        console.log('Sexta-Feira')
+        break
+    case 7:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Dia inválido')
+
+}
+
+// Exemplo 3
+
+let cor_exemplo3 = 'verde'
+
+switch (cor_exemplo3) {
+    case 'vermelho':
+        console.log('Cor é vermelho')
+    case 'verde':
+        console.log('Cor é verde')
+    case 'azul':
+        console.log('Cor é azul')
+    default:
+        console.log('Cor desconhecida')
+}
+
+
+// Exemplo 4
+
+let cor_exemplo4 = 'verde'
+
+switch (cor_exemplo4) {
+    case 'vermelho':
+        console.log('Cor é vermelho')
+        break
+    case 'verde':
+        console.log('Cor é verde')
+        break
+    case 'azul':
+        console.log('Cor é azul')
+        break
+    default:
+        console.log('Cor desconhecida')
+}
+
+
+// Exemplo 5
+
+let dia_exemplo5 = 4
+
+switch (dia_exemplo5) {
+    case 1:
+        console.log('Domingo')
+        break
+    case 2:
+        console.log('Segunda-Feira')
+        break
+    case 3:
+        console.log('Terça-Feira')
+        break
+    default:
+        console.log('Outro dia')
+
+}
+
+
+// Exemplo 6
+
+let dia_exemplo6 = 4
+
+switch (dia_exemplo6) {
+    case 1:
+        console.log('Domingo')
+        break
+    case 2:
+        console.log('Segunda-Feira')
+        break
+    case 3:
+        console.log('Terça-Feira')
+        break
+
+}
