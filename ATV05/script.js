a5a8d64016c79047ec95fdf6a278f1d95929a47f
Felipe <fgaf1ifal@gmail.com>
2026-05-25 16:58:43 -0300

    pastas

---
+++ ATV05/script.js
@@ -0,0 +1,20 @@
+function nome() {
+    var nome = prompt("Nome: ")
+    alert(`Seja bem vindo ${nome}!`)
+}
+
+
+function soma() {
+    var x = Number(prompt("Digite um número: "))
+    var y = Number(prompt("Digite outro número: "))
+
+    alert(`A soma resultante é: ${x + y}`)
+}
+
+function multiplicacao() {
+    var x = Number(prompt("Digite um número: "))
+    var y = Number(prompt("Digite outro número: "))
+    
+    console.log(`A multiplicação resultante é: ${x * y}`)
+}
+
---
+++ ATV08/script.js
@@ -0,0 +1,25 @@
+// Arrays
+
+let pessoa = []
+
+pessoa[0] = 'Carlos'
+pessoa[1] = 'Silva'
+pessoa[2] = 46
+
+
+
+let x = pessoa.length   // número de elementos do array
+let y = pessoa [0]
+
+console.log(x)
+console.log(y)
+
+
+
+let frutas = ['laranja', 'maçã', 'banana', 'uva', 'caju']
+
+console.log(frutas)
+console.log(frutas.length)
+
+
+frutas.push('pera')
